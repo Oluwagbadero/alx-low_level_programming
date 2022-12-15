5d89291e58b1594c79e38b8011f9e28a2908013c
Oluwagbadero <oluwagbaderoo@gmail.com>
2022-12-15 06:32:05 -0800

    Task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,11 @@
  * _isupper - uppercase letters
  * @c: char to check
  *
- * Return: On success 1.
+ * Return: 1 if is an uppercase character, 0 in other case. 
  */
 int _isupper(char c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '65' && c <= '90')
 		return (1);
 	else
 		return (0);
