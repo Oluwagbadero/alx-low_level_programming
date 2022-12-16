538b153a5f481e31280bdb5df467aecc0e390c17
Oluwagbadero <oluwagbaderoo@gmail.com>
2022-12-16 02:58:44 -0800

    Task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,16 @@
 /**
  * _isupper - uppercase letters
  * @c: char to check
- *
  * Return: 1 if is an uppercase character, 0 in other case. 
  */
 int _isupper(char c)
 {
-	if (c >= '65' && c <= '90')
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
