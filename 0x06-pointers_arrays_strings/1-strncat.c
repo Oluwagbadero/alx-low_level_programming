3db6d989ec1c8566b316939939ecd1e2c48d4091
Oluwagbadero <oluwagbaderoo@gmail.com>
2022-12-23 09:12:00 -0800

    Task 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 	}
 
-	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	for (co = 0; co < n; co++)
 	{
 		dest[i + co] = src[co];
 		if (src[co] == '\0')
